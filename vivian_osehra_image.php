3ac6c2cdc8d95830ae9b589418cb5b1e99052867
Jason Li <jason.li@kitware.com>
2014-07-23 12:41:49 -0400

    EHN: Add trademark (TM) symbol for VIVIAN

--- vivian_osehra_image.php
+++ vivian_osehra_image.php
@@ -4,9 +4,9 @@
 style="border-width:0" height="35" width="150" alt="OSEHRA Logo" /></a>
 </div>
   <div id="title" style="position:absolute; left:160px; top:10px; font-size:1.0em;">
-    VIVIAN (<u>Vi</u>sualizing <u>Vi</u>stA <u>A</u>nd <u>N</u>amespace)
+    VIVIAN<sup style="font-size:0.59em;">TM</sup>(<u>Vi</u>sualizing <u>Vi</u>stA <u>A</u>nd <u>N</u>amespace)
   </div>
-<div id="demoexamples" style="font-size:.9em; position:absolute; left:470px; top:2px">
+<div id="demoexamples" style="font-size:.9em; position:absolute; left:472px; top:2px">
   <ul class="nav nav-pills" role="navigation">
     <li><a href="index.php">Home</a></li>
     <li><a href="vista_menus.php">VistA Menus</a></li>
