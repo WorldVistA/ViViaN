cbed84fe2880a0f961949e321351f4c43363d20b
Betsy McPhail <betsy.mcphail@kitware.com>
2018-09-24 21:44:13 -0400

    Re-enable Stats bar chart and tests
    https://issues.osehra.org/browse/VIVIAN-342

--- test/test_pkg_dependency_chart.py
+++ test/test_pkg_dependency_chart.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #---------------------------------------------------------------------------
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -30,41 +31,38 @@ class test_pkgdepchart(unittest.TestCase):
 
   # TODO: This test was written for the circular graph, create an equivalent
   #       test for the bar chart
-  def DISABLE_test_01_chart_tooltip(self):
-    global driver
-    time.sleep(15)
-
-    ActionChains(driver).move_to_element(driver.find_element_by_link_text("Scheduling")).perform()
-    tooltip = driver.find_element_by_id("tooltip")
-    title = tooltip.find_element_by_id("header1").text
-
-    self.assertTrue(re.search("Name:", title))
-    self.assertTrue(re.search("Group:", "Patient Service Delivery"))
-
-    depends = tooltip.find_element_by_id("dependency").text
-    self.assertTrue(re.search("Depends:", depends))
-
-    dependents = tooltip.find_element_by_id("dependents").text
-    self.assertTrue(re.search("Dependents:", dependents))
-
-    dependents = tooltip.find_element_by_id("both").text
-    self.assertTrue(re.search("Both:", dependents))
+  def DISABLE_test_01_dep_bar_tooltip(self):
+    pass
 
   def test_02_dep_bar_content(self):
     global driver
 
-    # TODO: This test will pass even if there's no data!!
-    #       There will still be empty rectangles!
-    # Ensure that the list of data and labels isn't empty
+    # Ensure that the there is some data for both depends and dependents
     chart_container = driver.find_elements_by_class_name("highcharts-series")
-    for type in chart_container:
-      bar_array = type.find_elements_by_tag_name("rect")
-      self.assertTrue(len(bar_array) > 0)
+    for type in chart_container:  # depends, dependents
+      rects = type.find_elements_by_tag_name("rect")
+      for rect in rects:
+          if rect.get_attribute("height") > 0:
+              break
+      else:
+          self.fail("Failed to find data in Dependency Chart")
 
-    chart_container = driver.find_elements_by_class_name("highcharts-data-labels")
-    for type in chart_container:
-      bar_array = type.find_elements_by_tag_name("text")
-      self.assertTrue(len(bar_array) > 0)
+    # Check for data labels - make sure they exist and aren't all "0"
+    labels_list = driver.find_elements_by_class_name("highcharts-data-labels")
+    for type in labels_list:  # depends, dependents
+      labels = type.find_elements_by_tag_name("text")
+      for label in labels:
+          if label.text != "0":
+              break
+      else:
+          self.fail("Failed to find data labels in Dependency Chart")
+
+    # Ensure that each package is only included once
+    labels = driver.find_elements_by_class_name("highcharts-xaxis-labels")[1]
+    package_names = []
+    for item in labels.find_elements_by_xpath(".//a[@href]"):
+        package_names.append(str(item.get_attribute('innerHTML')))
+    self.assertEqual(len(package_names), len(set(package_names)), "Package names should be unique")
 
     # Read title of graph
     title_element = driver.find_element_by_class_name("highcharts-title")
@@ -79,10 +77,12 @@ class test_pkgdepchart(unittest.TestCase):
 
   def test_03_dep_bar_sort(self):
     global driver
+
     # Check each of the "Sort By" selections to ensure that
     # the first element is found in top_entries array
     dep_pulldown = driver.find_element_by_id("list-dep")
     pulldown_options = Select(dep_pulldown)
+
     # Name
     pulldown_options.select_by_visible_text("Name")
     time.sleep(5)
@@ -97,6 +97,88 @@ class test_pkgdepchart(unittest.TestCase):
     pulldown_options.select_by_visible_text("Dependents")
     time.sleep(5)
     self.assertEqual(driver.find_element_by_xpath("//*[@id='highcharts-0']/div/span[1]").text, "Kernel")
+
+  def test_04_bar_switch(self):
+    global driver
+
+    btn_group = driver.find_element_by_class_name("btn-group")
+    for btn in btn_group.find_elements_by_tag_name("label"):
+      type = btn.text.split(' ')[0]
+      btn.click()
+      self.assertTrue("active" in btn.get_attribute("class"))
+      time.sleep(5)
+
+    def DISABLE_test_05_stats_bar_tooltip(self):
+      pass
+
+    def test_06_stats_bar_content(self):
+      global driver
+
+      # Ensure that the there is some data for both routines, files and fields
+      chart_container = driver.find_elements_by_class_name("highcharts-series")
+      for type in chart_container:  # routines, files and fields
+        rects = type.find_elements_by_tag_name("rect")
+        for rect in rects:
+            if rect.get_attribute("height") > 0:
+                break
+        else:
+            self.fail("Failed to find data in Stats Chart")
+
+      # Check for data labels - make sure they exist and aren't all "0"
+      labels_list = driver.find_elements_by_class_name("highcharts-data-labels")
+      for type in labels_list:  # routines, files and fields
+        labels = type.find_elements_by_tag_name("text")
+        for label in labels:
+            if label.text != "0":
+                break
+        else:
+            self.fail("Failed to find data labels in Stats Chart")
+
+      # Ensure that each package is only included once
+      labels = driver.find_elements_by_class_name("highcharts-xaxis-labels")[1]
+      package_names = []
+      for item in labels.find_elements_by_xpath(".//a[@href]"):
+          package_names.append(str(item.get_attribute('innerHTML')))
+      self.assertEqual(len(package_names), len(set(package_names)), "Package names should be unique")
+
+      # Read title of graph
+      title_element = driver.find_element_by_class_name("highcharts-title")
+      self.assertEqual(title_element.text, 'VistA Package Statistics')
+
+      # Ensure that legends have proper information
+      stat_chart_legend = ['routines','files','fields']
+      chart_legend = driver.find_element_by_class_name("highcharts-legend")
+      legend_array = chart_legend.find_elements_by_tag_name("text")
+      for item in legend_array:
+         self.assertTrue(item.get_attribute("innerHTML") in stat_chart_legend)
+
+    def test_03_dep_bar_sort(self):
+      global driver
+
+      # Check each of the "Sort By" selections to ensure that
+      # the first element is found in top_entries array
+      dep_pulldown = driver.find_element_by_id("list-dep")
+      pulldown_options = Select(dep_pulldown)
+
+      # Name
+      pulldown_options.select_by_visible_text("Name")
+      time.sleep(5)
+      self.assertEqual(driver.find_element_by_xpath("//*[@id='highcharts-0']/div/span[1]").text, "A1BF Response Time")
+
+      # Routines
+      pulldown_options.select_by_visible_text("Routines")
+      time.sleep(5)
+      self.assertEqual(driver.find_element_by_xpath("//*[@id='highcharts-0']/div/span[1]").text, "Automated Information Collection System")
+
+      # Files
+      pulldown_options.select_by_visible_text("Files")
+      time.sleep(5)
+      self.assertEqual(driver.find_element_by_xpath("//*[@id='highcharts-0']/div/span[1]").text, "Integrated Billing")
+
+      # Fields
+      pulldown_options.select_by_visible_text("File Fields")
+      time.sleep(5)
+      self.assertEqual(driver.find_element_by_xpath("//*[@id='highcharts-0']/div/span[1]").text, "Integrated Billing")
 
 
 if __name__ == '__main__':
